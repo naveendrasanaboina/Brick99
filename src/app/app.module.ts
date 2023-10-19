dc072299b6028d8d29297f395d0b55195bb81859
nacveendra <sanaboinanaveendra@gmail.com>
2023-10-19 20:01:14 +0530

    deleted index component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
 import { AboutComponent } from './about/about.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
     AboutComponent,
     UserPanelComponent
   ],
