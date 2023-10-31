682a27ba0ef8f74aae9140bc02af55e9a49d346b
nacveendra <sanaboinanaveendra@gmail.com>
2023-10-31 21:28:39 +0530

    added services component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    UserPanelComponent
+    UserPanelComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
